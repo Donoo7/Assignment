94c9cdbd12cc344619d57a38f1754d3ee3a13e84
Donoo7 <158152092+Donoo7@users.noreply.github.com>
2024-02-24 12:11:08 +0530

    Add files via upload

---
+++ assignments/Day1.py
@@ -0,0 +1,51 @@
+# 1 Write a python program to calculate the factorial of a give number
+# num = int(input("Enter a number: "))
+# fact = 1
+# for i in range (1,num+1):
+#     fact = fact*i
+# print("factorial of ",num, "is ", fact)
+
+# 3 Write a pyhton program to print the number from 1 to 10
+# for i in range(1,11):
+#     print(i)
+
+# 4 Write a python program to print multiplication table of a give number
+# num1 = int(input("Enter a number :"))
+# for i in range(1,11):
+#     print(num1 * i)
+#     if(i==10):
+#         break
+
+# 5 Write a program to calculate the factorial of a given number using the math module
+# import math
+# num2 = int(input("Enter a number : "))
+# print(math.factorial(num))
+
+# 6 Write a program to calculate the square root of a given number using the math module
+# import math
+# num3 = int(input("Enter a number : "))
+# print(math.sqrt(num3))
+
+# 7 Write a python program to shuffle a list using the random module
+# import random
+# L = [1,34,22,67,10,0,100,5,54,35,78,6,9,99]
+# random.shuffle(L)
+# print(L)
+
+# 8 Write a python program to generate a random floating-point number between 0 and 1
+# import random
+# random.seed(0,1)
+# print(random.random())
+
+# 9 How do you determine if a year is a leap year in pyhton? Write a python program to cheak leap year or not.
+# import calendar
+# num4 = int(input("Enter a year :"))
+# leap = calendar.isleap(num4)
+# if(leap==False):
+#     print(num4, " is not a leap year.")
+# else:
+#     print(num4, " is a leap year.")
+
+# 10 Write a python program to check if a string starts with a specific prefix
+str1 = "This is a beautiful day in the month of december."
+"this" in "this"
